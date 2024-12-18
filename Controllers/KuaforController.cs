7ff68bb51dc348044b6d597e243e6d5f11a3d9af
Egemen Bozca <egemen.bozca@ogr.sakarya.edu.tr>
2024-12-18 22:58:06 +0300

    Layout düzenlemesi

--- Controllers/KuaforController.cs
+++ Controllers/KuaforController.cs
@@ -18,55 +18,5 @@ namespace Web_Odev.Controllers
             var kuaforler = _context.Kuaforler.ToList();
             return View(kuaforler);
         }
-
-        public IActionResult Ekle()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Ekle(Kuafor kuafor)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Kuaforler.Add(kuafor);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(kuafor);
-        }
-
-        public IActionResult Duzenle(int id)
-        {
-            var kuafor = _context.Kuaforler.Find(id);
-            if (kuafor == null)
-            {
-                return NotFound();
-            }
-            return View(kuafor);
-        }
-
-        [HttpPost]
-        public IActionResult Duzenle(Kuafor kuafor)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Kuaforler.Update(kuafor);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(kuafor);
-        }
-
-        public IActionResult Sil(int id)
-        {
-            var kuafor = _context.Kuaforler.Find(id);
-            if (kuafor != null)
-            {
-                _context.Kuaforler.Remove(kuafor);
-                _context.SaveChanges();
-            }
-            return RedirectToAction("Index");
-        }
     }
 }
